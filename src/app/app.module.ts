7471f9854fa65c00d47ba7c7a020431816a031e5
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-21 23:46:33 -0500

    se agrego estilo a cada card y se creo componente heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 import { HomeComponent } from "./components/home/home.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { AboutComponent } from "./components/about/about.component";
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { AboutComponent } from "./components/about/about.component";
     HomeComponent,
     HeroesComponent,
     AboutComponent,
+    HeroeComponent,
   ],
   imports: [BrowserModule, APP_ROUTING],
   providers: [HeroesService],
