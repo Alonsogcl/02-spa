ba63b734908c6b7504d0d1ccfac6f2e4ebc2b98f
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-22 23:56:19 -0500

    recibiendo parametros por url activatedroute

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';//Se usa igual que el servicio en el constructor
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe:any={};//variable local 
 
-  ngOnInit(){
-  }
-
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService:HeroesService
+    ) { 
+    this.activatedRoute.params.subscribe(params =>{
+    console.log(params['id']); //este id es xq en el route se puso este id
+    //para obtener el heroe en particular con este id es necesario modificar nuestro servicio
+    this.heroe= this._heroesService.getHeroe(params['id']);
+  });
+}
 }
