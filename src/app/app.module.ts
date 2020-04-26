22ba371d30b7e6992653ccc9a497331253b79799
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-26 13:50:21 -0500

    componente de busqueda agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from "./components/home/home.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { AboutComponent } from "./components/about/about.component";
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { HeroeComponent } from './components/heroe/heroe.component';
     HeroesComponent,
     AboutComponent,
     HeroeComponent,
+    BuscadorComponent,
   ],
   imports: [BrowserModule, APP_ROUTING],
   providers: [HeroesService],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroeComponent } from "./components/heroe/heroe.component";
+import { BuscadorComponent } from "./components/buscador/buscador.component";
 
 const APP_ROUTES: Routes = [
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "heroes", component: HeroesComponent },
+  { path: "buscar", component: BuscadorComponent },
   { path: "heroe/:id", component: HeroeComponent },
   /*ruta que va a recibir un parametro el id de cada heroe que se quiere mostrar cuando se pase el parametro por la url */
   { path: "**", pathMatch: "full", redirectTo: "home" },
