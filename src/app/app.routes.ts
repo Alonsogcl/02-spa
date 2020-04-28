c96a14cd107cd9f65bcb22fbe3668dad3702d455
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-04-27 23:39:31 -0500

    configuración del componente buscar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ const APP_ROUTES: Routes = [
   { path: "home", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "heroes", component: HeroesComponent },
-  { path: "buscar", component: BuscadorComponent },
+  { path: "buscar/:termino", component: BuscadorComponent },
   { path: "heroe/:id", component: HeroeComponent },
   /*ruta que va a recibir un parametro el id de cada heroe que se quiere mostrar cuando se pase el parametro por la url */
   { path: "**", pathMatch: "full", redirectTo: "home" },
--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -68,19 +68,6 @@ export class HeroesService{
       return this.heroes[idx];
     }
 
-    buscarHeroes(termino: string): Heroe[] {
-      let heroesArr: Heroe[] = [];
-      termino = termino.toLowerCase();
-      //Por cada uno de los heroes lo va a verificar todo
-      for (let heroe of this.heroes) { //barrido de todo nuestro arreglo de los heroes para ver si alguno tiene el nombre que estamos buscando, barrer todos los heroes que tengo en el servicio 
-        let nombre = heroe.nombre.toLowerCase(); //por cada iteración voy a extraer el nombre de cada heroe 
-
-        if (nombre.indexOf(termino) >= 0) { //si el nombre de la iteracion de mi heroe (indexof me permitira buscar un string dentro del nombre) Si se encuentra el termino en ese nombre va a regresar la posicion donde la encuentra un 0 o superior sino lo encuentra regresa un -1 , mayor q 0 si encontro el termino
-          heroesArr.push(heroe); //Agregar ese heroe a mi nuevo de arreglos temporal xq no queremos modificar la data del arreglo
-        }
-      }
-      return heroesArr; //Regresa un arreglo de Heroe
-    }
 }
     export interface Heroe{
         nombre: string;
