fcdadb1227ef763843322b6f788c3630cec621d2
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-05-05 17:01:22 -0500

    componente card creado, recibiendo información de componente padre a hijo

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeTarjetaComponent implements OnInit {
 
-  constructor() { }
+  @Input() heroe: any = {}; //indica que esta propiedad va a ser recibida desde afuera
+  //heroe se puede inicializar desde el constructor y si no recibe un valor desde afuera se pondrá lo que inicialice en el constructor
+  //Al poner el decorador Input me habilita que a donde vaya a utilizar esta etiqueta html 
+  //puedo mandarle argumentos desde el padre hasta allá utilizando el mismo nombre que se le puso a la propiedad heroe
+  //si se pone heroe entre llaves cuadradas [heroe] puedo mandar el valor de una variable, una expresión a ese componente.
+  @Input() index: number; //se va a recibir mediante los argumentos que se encuentran afuera de este componente
+  //lo recibirá de heroes.component.html
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  verHeroe() {
+    console.log(this.index);
+    this.router.navigate(["/heroe", this.index]);
+  }
 }
