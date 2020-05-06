42ca12b76abdaad0a3a1a9eddd7bfb982620e2bf
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-05-05 23:44:53 -0500

    emitiendo evento del hijo al padre con output

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -16,13 +16,20 @@ export class HeroeTarjetaComponent implements OnInit {
   //si se pone heroe entre llaves cuadradas [heroe] puedo mandar el valor de una variable, una expresión a ese componente.
   @Input() index: number; //se va a recibir mediante los argumentos que se encuentran afuera de este componente
   //lo recibirá de heroes.component.html
-  constructor(private router:Router) { }
+
+  @Output() heroeSeleccionado: EventEmitter<number>;//nombre del evento que yo quiero que el padre esté escuchando, hay que especificar que tipo de información va a emitir esta función
+  //si va a emitir un objeto un booleano, string, en este caso se quiere emitir un tipo number porque quiero mandar el index
+  constructor(private router:Router) { 
+    this.heroeSeleccionado= new EventEmitter(); //Hay que inicializar este evento emitter
+    //lo que se quiere es que dispare un evento personalizado
+  }
 
   ngOnInit(): void {
   }
 
   verHeroe() {
-    console.log(this.index);
-    this.router.navigate(["/heroe", this.index]);
+   // console.log(this.index);
+   // this.router.navigate(["/heroe", this.index]);
+   this.heroeSeleccionado.emit(this.index); //Que emita el indice que estoy recibiendo, se puede emitir cualquier cosa
   }
 }
